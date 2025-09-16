c3dc9825f41616854f262c4b5b52c7c70b84370f
Luca Strano <lucastrano2111@icloud.com>
2025-09-17 00:14:28 +0200

    Add entry point for the backend

---
+++ backend/src/main/java/com/isdstore/ISDStoreApplication.java
@@ -0,0 +1,12 @@
+package com.isdstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Entry point for the Backend
+@SpringBootApplication
+public class ISDStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ISDStoreApplication.class, args);
+    }
+}
